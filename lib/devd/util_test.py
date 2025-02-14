adb30fb82d0232d9f44d39c1455b5f7a6b796ff8
Kurt Stephens <git@kurtstephens.com>
2025-02-13 18:28:21 -0600

    test_with_timing.

--- lib/devd/util_test.py
+++ lib/devd/util_test.py
@@ -1,3 +1,5 @@
+from time import sleep
+from datetime import datetime
 from . import util as sut
 
 
@@ -5,3 +7,25 @@ def test_pairs():
     items = (1, 2, 3)
     assert sut.pairs(items) == [(1, 2), (1, 3), (2, 3)]
     assert sut.pairs(items, 0) == [(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3)]
+
+
+def test_with_timing():
+    def f():
+        sleep(0.5)
+        return 2
+
+    result, exc, t0, t1, dt_sec = sut.with_timing(f)
+    assert result == 2
+    assert exc is None
+    assert isinstance(t0, datetime)
+    assert isinstance(t1, datetime)
+    assert t1 > t0
+    assert dt_sec > 0.4
+
+    def g():
+        sleep(0.5)
+        raise ValueError("nope")
+
+    result, exc, t0, t1, dt_sec = sut.with_timing(g)
+    assert result is None
+    assert isinstance(exc, ValueError)
